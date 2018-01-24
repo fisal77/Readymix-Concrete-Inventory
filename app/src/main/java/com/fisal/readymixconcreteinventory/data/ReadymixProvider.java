2ed81022583327ee42911e7d270fdc9425052e01
fisal77 <fisal77@hotmail.com>
2018-01-24 10:59:06 +0300

    Add UriMatcher to ContentProvider

--- app/src/main/java/com/fisal/readymixconcreteinventory/data/ReadymixProvider.java
+++ app/src/main/java/com/fisal/readymixconcreteinventory/data/ReadymixProvider.java
@@ -2,6 +2,7 @@ package com.fisal.readymixconcreteinventory.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -13,6 +14,40 @@ import android.net.Uri;
  * {@link ContentProvider} for Readymix comcrete products app.
  */
 public class ReadymixProvider extends ContentProvider {
+
+    /** URI matcher code for the content URI for the readymixes table */
+    private static final int READYMIX = 1000;
+
+    /** URI matcher code for the content URI for a single readymix in the readymix table */
+    private static final int READYMIX_ID = 1001;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+        // The content URI of the form "content://com.fisal.readymixconcreteinventory/readymix" will map to the
+        // integer code {@link #READYMIX}. This URI is used to provide access to MULTIPLE rows
+        // of the readymix table.
+        sUriMatcher.addURI(ReadymixContract.CONTENT_AUTHORITY, ReadymixContract.PATH_READYMIX, READYMIX);
+
+        // The content URI of the form "content://com.fisal.readymixconcreteinventory/readymix/#" will map to the
+        // integer code {@link #READYMIX_ID}. This URI is used to provide access to ONE single row
+        // of the pets table.
+        //
+        // In this case, the "#" wildcard is used where "#" can be substituted for an integer.
+        // For example, "content://com.fisal.readymixconcreteinventory/readymix/7" matches, but
+        // "content://com.fisal.readymixconcreteinventory/readymix" (without a number at the end) doesn't match.
+        sUriMatcher.addURI(ReadymixContract.CONTENT_AUTHORITY, ReadymixContract.PATH_READYMIX + "/#", READYMIX_ID);
+    }
 
     /** Database helper object */
     private ReadymixDbHelper mDbHelper;
