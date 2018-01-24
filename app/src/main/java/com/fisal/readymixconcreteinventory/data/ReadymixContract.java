13487b93986d5c02a1669e826efb1882a60405b2
fisal77 <fisal77@hotmail.com>
2018-01-24 10:27:12 +0300

    Add constants for readymix content URIs

--- app/src/main/java/com/fisal/readymixconcreteinventory/data/ReadymixContract.java
+++ app/src/main/java/com/fisal/readymixconcreteinventory/data/ReadymixContract.java
@@ -1,5 +1,6 @@
 package com.fisal.readymixconcreteinventory.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -12,10 +13,35 @@ public final class ReadymixContract {
     private ReadymixContract() {}
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.fisal.readymixconcreteinventory";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.fisal.readymixconcreteinventory/readymix/ is a valid path for
+     * looking at pet data. content://com.fisal.readymixconcreteinventory/account/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "account".
+     */
+    public static final String PATH_READYMIX = "readymix";
+
+    /**
      * Inner class that defines constant values for the Readymix database table.
      * Each entry in the table represents a single Readymix.
      */
     public static final class ReadymixEntry implements BaseColumns {
+
+        /** The content URI to access the readymix data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_READYMIX);
 
         /** Name of database table for Readymix */
         public final static String TABLE_NAME = "readymix";
